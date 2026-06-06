5ea9b83717616de0a19e43dcd034fa50def2dd71
sachin-dev45 <sachinwaghmare00731@gmail.com>
2026-06-06 13:49:34 +0530

    Update UserController.java

--- user-service/src/main/java/com/shopping/user/controller/UserController.java
+++ user-service/src/main/java/com/shopping/user/controller/UserController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final AuthService authService;
